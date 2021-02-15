51634a8dd9530bf1032501628c14a41c8917fdae
SarahAlAshwal <sa.alashwal@gmail.com>
2021-02-14 20:59:49 -0500

    create todo App

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import Header from "./components/Header.js";
+import TodoItem from "./components/TodoItem.js";
+import AddTodo from "./components/addTodo.js";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
+  ]);
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("OOOPS!", "Todo must be at least 3 characters!", [
+        {
+          text: "Understood",
+          onPress: () => {
+            console.log("alert closed");
+          },
+        },
+      ]);
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}>
+        <Header></Header>
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  content: {
+    flex: 1,
+    padding: 40,
+  },
+  list: {
+    flex: 1,
+    marginTop: 20,
   },
 });
+
+///android-studio-ide-201.7042882-linux/android-studio/bin$ ./studio.sh
+
+//npm start
---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+
+export default function Header() { 
+  return (
+    <View style={style.header}>
+      <Text style={style.title}>My Todos</Text>
+    </View>
+  );
+}
+  const style = StyleSheet.create({
+
+    header: {
+      height: 80,
+      paddingTop: 38,
+      backgroundColor: 'coral',
+    },
+    title: {
+      textAlign: 'center',
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 20,
+    }
+
+  });
